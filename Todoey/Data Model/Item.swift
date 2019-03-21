689e9fb69da5f508e3a22d164f38cd47b46357a0
justin.fang <justin.fang@dongmancorp.cn>
2019-03-21 11:43:13 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Justin Fang on 2019/3/21.
+//  Copyright © 2019 Justin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
